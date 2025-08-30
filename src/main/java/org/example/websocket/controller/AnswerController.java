27e7475a890cacb1196d4a37b89c0f14d2682a9d
= <=>
2025-08-30 15:44:15 +0530

    docker file added

--- src/main/java/org/example/websocket/controller/AnswerController.java
+++ src/main/java/org/example/websocket/controller/AnswerController.java
@@ -1,6 +1,5 @@
 package org.example.websocket.controller;
 
-import org.apache.http.HttpEntity;
 import org.example.websocket.model.RecordsOfQNA;
 import org.example.websocket.model.User;
 import org.example.websocket.repository.QNARepository;
@@ -8,20 +7,12 @@ import org.example.websocket.service.GeminiService;
 import org.example.websocket.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Map;
-<<<<<<< HEAD
-
-=======
->>>>>>> d02ef0e6f8b9a61ac4e70bb96735637b9576ce20
 
 @RestController
 @RequestMapping("/public/answer")
@@ -35,7 +26,6 @@ public class AnswerController {
     @Autowired
     private RestTemplate restTemplate;
 
-<<<<<<< HEAD
     @Autowired
     private GeminiService geminiService;
 
@@ -45,8 +35,6 @@ public class AnswerController {
     private static final String GEMINI_API_URL =
             "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent";
 
-=======
->>>>>>> d02ef0e6f8b9a61ac4e70bb96735637b9576ce20
     @PostMapping("/save")
     public void saveAnswers(@RequestBody List<String> answers) {
 
